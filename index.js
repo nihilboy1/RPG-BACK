5ee21a8f413b111a241167e681aa41bbec499bc0
Samuel Seve <62472120+nihilboy1455@users.noreply.github.com>
2022-03-18 11:05:55 -0300

    CORS OPTIONS

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express()
 require('dotenv').config()
+const cors = require("cors")
+
+const options = {
+  origin: "http://localhost:3000"
+}
+
+app.use(cors(options))
 
 const txt = {
   age1: 'txt1',
@@ -12,14 +19,6 @@ app.get('/api/txt', (req, res) => {
   res.send(txt)
 })
 
-app.get('/google', (req, res) => {
-  fetch(
-    'https://docs.googleapis.com/v1/documents/1I_QfeXP6uqdsySQOlYjgT55SRV6U4P4O9HIIrMbJ-MM'
-  ).then(res => {
-    console.log(res)
-    return res.json()
-  })
-})
 
 app.listen(process.env.PORT || 5000, () => {
   console.log('Rodando na porta 5000')
